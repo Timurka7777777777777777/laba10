6f713221ea5e579b71f0a70fb88d01d0896933ec
Тимур <timur108@bk.ru>
2023-12-15 17:43:46 +0300

    Практическая 10

---
+++ app/src/main/java/com/example/myapplication/Activity1.kt
@@ -0,0 +1,52 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Activity1 : AppCompatActivity() {
+    val colors = arrayListOf(0xC5CAE9, 0xB2EBF2, 0xFFF9C4)
+    var sheetNumber = 0
+    val text: EditText = findViewById(R.id.text)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity1)
+        sheetNumber = getIntent().getIntExtra("sheetNumber", 0)
+        val next:Button=findViewById(R.id.next)
+        next.setOnClickListener(){
+            if (sheetNumber < colors.size - 1){
+            val intent = Intent(this,Activity1::class.java)
+                intent.putExtra("sheetNimber", sheetNumber + 1)
+            startActivity(intent)
+        }
+            else {
+                Toast.makeText(applicationContext, getString(R.string.textPersonName),Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        val prefs = getPreferences(Context.MODE_PRIVATE).edit()
+
+        prefs.putString("note1",text.editableText.toString())
+        prefs.apply()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val sheet : ConstraintLayout = findViewById(R.id.sheet)
+        sheet.setBackgroundColor(colors[sheetNumber].toInt())
+        val saveState = getPreferences(Context.MODE_PRIVATE).getString("note1",null)
+        if (saveState !=null)
+            text.setText(saveState)
+    }
+}
